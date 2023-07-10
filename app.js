766b95aa367ca93e2f41ce2f49edd2398b252a7b
Je Dela Cruz <93860350+jedelacruz@users.noreply.github.com>
2023-07-11 04:01:17 +0800

    Update app.js

--- app.js
+++ app.js
@@ -130,3 +130,18 @@ for (var i = 0; i < btns.length; i++) {
     this.className += " active";
   });
 }
+
+let btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
